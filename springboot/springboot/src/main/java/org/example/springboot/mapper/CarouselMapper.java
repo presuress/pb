c871e0718a623410c4d1682e860df2ee420bb03d
lyc <66305820@qq.com>
2025-12-09 17:10:37 +0800

    all

--- springboot/springboot/src/main/java/org/example/springboot/mapper/CarouselMapper.java
+++
@@ -1,9 +0,0 @@
-package org.example.springboot.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.example.springboot.entity.Carousel;
-
-@Mapper
-public interface CarouselMapper extends BaseMapper<Carousel> {
-} 
--- springboot/springboot/src/main/java/org/example/springboot/mapper/TransactionMapper.java
+++
@@ -1,9 +0,0 @@
-package org.example.springboot.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.example.springboot.entity.Transaction;
-
-@Mapper
-public interface TransactionMapper extends BaseMapper<Transaction> {
-} 
